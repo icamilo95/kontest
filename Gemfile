3083f95197595020fe065637f1825fea7c10d53d
Camilo Ordonez <icoj95@gmail.com>
2015-10-13 23:49:35 -0700

    added rails admin, admin field is being filled through console

--- Gemfile
+++ Gemfile
@@ -25,9 +25,13 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-gem "devise"
+
 # Use Unicorn as the app server
 # gem 'unicorn'
+
+gem "devise"
+gem "rails_admin"
+
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013025402) do
+ActiveRecord::Schema.define(version: 20151014053453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20151013025402) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean  "admin"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
